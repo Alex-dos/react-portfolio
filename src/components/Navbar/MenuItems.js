97eb1c249f62e9a7c47dd8010b442b381ff1eda8
Alexandre Dosseto <leonedimars@gmail.com>
2021-03-25 15:38:32 +0100

    Init navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Project",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Bio",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Links",
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: "Contact",
+        url: '#',
+        cName: 'nav-links'
+    }
+]
